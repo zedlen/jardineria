7252960ab0f90ae45c321acba51755dac7b2624f
Luis <lmelendez@pointverde.com>
2017-12-07 00:19:54 -0600

    make it better

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/jardinera/';
+$config['base_url'] = 'https://jardinera.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
