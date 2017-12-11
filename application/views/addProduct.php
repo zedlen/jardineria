19da3a7086e9b67b3f3164426b6c92fed12868d5
Luis <lmelendez@pointverde.com>
2017-12-10 22:43:56 -0600

    MEnsajes de esperando y success

--- application/views/addProduct.php
+++ application/views/addProduct.php
@@ -9,7 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="<?php echo base_url();?>bower_components/sweetalert2/dist/sweetalert2.all.min.js"></script>
+	<script src="<?php echo base_url();?>bower_components/sweetalert2/dist/sweetalert2.all.js"></script>
 	<script src="<?php echo base_url();?>bower_components/bootstrap-waitingfor/build/bootstrap-waitingfor.js"></script>
 
 	<!-- Include a polyfill for ES6 Promises (optional) for IE11 and Android browser -->
